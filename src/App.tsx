1da96ed6e1465ba72921db8513b39650f708071f
Arda Karamoçu <arda.karamocu2@gmail.com>
2025-07-03 01:26:31 +0300

    Initial commit - Social Proof Section project

---
+++ src/App.tsx
@@ -0,0 +1,182 @@
+function App() {
+  return (
+    <>
+      <div className="font-League-Spartan p-5 lg:px-24 lg:py-14">
+        <div className="flex flex-col lg:flex-row justify-between items-center gap-10 mb-14">
+          <div>
+            <h1 className="text-4xl lg:text-5xl text-[hsl(300,43%,22%)] font-[700] lg:max-w-md text-center lg:text-left">
+              10,000+ of our users love our products.
+            </h1>
+            <p className="text-lg text-[hsl(303,10%,53%)] font-[400] text-center lg:text-left">
+              We only provide great products combined with excellent customer
+              service. See what our satisfied customers are saying about our
+              services.
+            </p>
+          </div>
+          <div className="flex flex-col gap-y-4 w-full self-end items-end">
+            <div className="flex flex-col w-full lg:w-[380px] lg:flex-row lg:mr-20 gap-x-4 px-6 py-4 rounded-xl bg-[hsl(300,24%,96%)] items-center">
+              <div className="flex flex-row gap-x-2">
+                <img
+                  src="src/images/icon-star.svg"
+                  alt=""
+                  className="w-4 h-4 "
+                />
+                <img
+                  src="src/images/icon-star.svg"
+                  alt=""
+                  className="w-4 h-4 "
+                />
+                <img
+                  src="src/images/icon-star.svg"
+                  alt=""
+                  className="w-4 h-4 "
+                />
+                <img
+                  src="src/images/icon-star.svg"
+                  alt=""
+                  className="w-4 h-4 "
+                />
+                <img
+                  src="src/images/icon-star.svg"
+                  alt=""
+                  className="w-4 h-4 "
+                />
+              </div>
+              <h2 className="text-[hsl(300,43%,22%)] text-md font-[700]">
+                Rated 5 Stars in Reviews
+              </h2>
+            </div>
+            <div className="flex flex-col w-full lg:flex-row gap-x-4 px-5 py-4 rounded-xl bg-[hsl(300,24%,96%)] lg:mr-10 items-center lg:w-[380px]">
+              <div className="flex flex-row gap-x-2">
+                <img
+                  src="src/images/icon-star.svg"
+                  alt=""
+                  className="w-4 h-4 "
+                />
+                <img
+                  src="src/images/icon-star.svg"
+                  alt=""
+                  className="w-4 h-4 "
+                />
+                <img
+                  src="src/images/icon-star.svg"
+                  alt=""
+                  className="w-4 h-4 "
+                />
+                <img
+                  src="src/images/icon-star.svg"
+                  alt=""
+                  className="w-4 h-4 "
+                />
+                <img
+                  src="src/images/icon-star.svg"
+                  alt=""
+                  className="w-4 h-4 "
+                />
+              </div>
+              <h2 className="text-[hsl(300,43%,22%)] text-md font-[700]">
+                Rated 5 Stars in Report Guru
+              </h2>
+            </div>
+            <div className="flex flex-col w-full lg:flex-row gap-x-4 px-5 py-4 rounded-xl bg-[hsl(300,24%,96%)] items-center lg:w-[380px]">
+              <div className="flex flex-row gap-x-2">
+                <img
+                  src="src/images/icon-star.svg"
+                  alt=""
+                  className="w-4 h-4 "
+                />
+                <img
+                  src="src/images/icon-star.svg"
+                  alt=""
+                  className="w-4 h-4 "
+                />
+                <img
+                  src="src/images/icon-star.svg"
+                  alt=""
+                  className="w-4 h-4 "
+                />
+                <img
+                  src="src/images/icon-star.svg"
+                  alt=""
+                  className="w-4 h-4"
+                />
+                <img
+                  src="src/images/icon-star.svg"
+                  alt=""
+                  className="w-4 h-4"
+                />
+              </div>
+              <h2 className="text-[hsl(300,43%,22%)] text-md font-[700]">
+                Rated 5 Stars in BestTech
+              </h2>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col lg:flex-row gap-4 lg:gap-6 justiny-center items-end">
+          <div className="bg-[hsl(300,43%,22%)] rounded-lg flex flex-col gap-y-4 p-7 lg:mb-20 lg:h-60">
+            <div className="flex flex-row gap-x-4 items-center">
+              <img
+                src="src/images/image-colton.jpg"
+                alt="Colton Smith"
+                className="rounded-full w-8 h-8"
+              />
+              <div className="flex flex-col">
+                <h3 className="text-white font-[500]">Colton Smith</h3>
+                <h4 className="text-[hsl(333,80%,67%)] font-[300]">
+                  Verified Buyer
+                </h4>
+              </div>
+            </div>
+            <p className="text-white">
+              "We needed the same printed design as the one we had ordered a
+              week prior. Not only did they find the original order, but we also
+              received it in time. Excellent!"
+            </p>
+          </div>
+          <div className="bg-[hsl(300,43%,22%)] rounded-lg flex flex-col gap-y-4 p-7 lg:mb-10 lg:h-60">
+            <div className="flex flex-row gap-x-4 items-center">
+              <img
+                src="src/images/image-irene.jpg"
+                alt="Irene Roberts"
+                className="rounded-full w-8 h-8"
+              />
+              <div className="flex flex-col">
+                <h3 className="text-white font-[500]">Irene Roberts</h3>
+                <h4 className="text-[hsl(333,80%,67%)] font-[300]">
+                  Verified Buyer
+                </h4>
+              </div>
+            </div>
+            <p className="text-white">
+              "Customer service is always excellent and very quick turn around.
+              Completely delighted with the simplicity of the purchase and the
+              speed of delivery."
+            </p>
+          </div>
+          <div className="bg-[hsl(300,43%,22%)] rounded-lg flex flex-col gap-y-4 p-7 lg:h-60">
+            <div className="flex flex-row gap-x-4 items-center">
+              <img
+                src="src/images/image-anne.jpg"
+                alt="Anne Wallace"
+                className="rounded-full w-8 h-8"
+              />
+              <div className="flex flex-col">
+                <h3 className="text-white font-[500]">Anne Wallace</h3>
+                <h4 className="text-[hsl(333,80%,67%)] font-[300]">
+                  Verified Buyer
+                </h4>
+              </div>
+            </div>
+            <p className="text-white">
+              "Put an order with this company and can only praise them for the
+              very high standard. Will definitely use them again and recommend
+              them to everyone!"
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
